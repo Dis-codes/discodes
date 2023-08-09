cd3bbfd3e0f39691b754a98d5f2e537d5364bbd2
noxyyk <setunskyto@gmail.com>
2023-08-09 15:52:35 +0200

    change log

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -2,13 +2,9 @@
     import Changelog from './changelog/+page.svelte';
   </script>
 <div class="flex flex-col items-center justify-center h-screen">
-    <h1 class="text-3xl font-bold underline text-[25px]">Discodes</h1>
-    <h3 class="text-center font-bold">The best place for your discord bot</h3>
-    <div class="flex gap-2 mt-4">
-        <a href="/login" class="bg-zinc-900 text-white hover:bg-white hover:text-zinc-900 rounded-full px-4 py-1 transition duration-300">Login</a>
-        <a href="/register" class="bg-zinc-900 text-white hover:bg-white hover:text-zinc-900 rounded-full px-4 py-1 transition duration-300">New here?</a>
-      </div>
-    <button class="mt-4 rounded-full bg-blue-500 text-white hover:bg-white hover:text-blue-500 px-4 py-2 transition duration-300">Start</button>
+    <h1 class="text-3xl font-bold underline text-[45px]">Discodes</h1>
+    <h3 class="text-center font-bold mt-4">The best place for your discord bot</h3>
+      <a href="/login" class="mt-8 rounded-full bg-blue-500 text-white hover:bg-white hover:text-blue-500 px-20 py-2 transition duration-300">Start</a>
+    <div class="py-10" />
+    <Changelog />
   </div>
-
-  <Changelog />
--- app/src/routes/changelog/+page.svelte
+++ app/src/routes/changelog/+page.svelte
@@ -1,1 +1,46 @@
-<h1>test</h1>
+<script>
+    export let logs = [
+      { name: "test1", image: "https://amoeboids.com/wp-content/uploads/2020/02/Pointers-for-making-a-good-changelog.webp", description: "test1", author: "Noxyyk", time:"9.8.2023"},
+      { name: "test2", image: "https://amoeboids.com/wp-content/uploads/2020/02/Pointers-for-making-a-good-changelog.webp", description: "test2", author: "Noxyyk", time:"9.8.2023" },
+      { name: "test3", image: "https://amoeboids.com/wp-content/uploads/2020/02/Pointers-for-making-a-good-changelog.webp", description: "test3", author: "Noxyyk", time:"9.8.2023" }
+    ];
+  </script>
+  
+  <div class="changelog-container w-1/2 mx-auto">
+    {#each logs as log (log.name)}
+      <div class="log-item border p-4 my-2 rounded relative">
+        <h4 class="text-lg font-bold mb-2 text-center">{log.name}</h4>
+        <img class="w-full h-auto max-h-80 object-contain mb-2" src={log.image} alt={log.name} />
+        <p>{log.description}</p>
+        <div class="absolute bottom-0 right-0 text-sm text-gray-500 mr-1">
+            Posted by <span class="font-bold">{log.author}</span> at {log.time}
+          </div>
+      </div>
+    {/each}
+  </div>
+  
+  
+  <style>
+    .changelog-container {
+      max-height: 50vh;
+      overflow-y: auto;
+  
+      /* Custom scroll bar styles */
+      scrollbar-width: thin;
+      scrollbar-color: transparent  rgb(209 213 219);
+  
+      &::-webkit-scrollbar {
+        width: 8px;
+      }
+  
+      &::-webkit-scrollbar-thumb {
+        background-color:  rgb(209 213 219);
+        border-radius: 4px;
+      }
+  
+      &::-webkit-scrollbar-track {
+        background-color: transparent;
+      }
+    }
+  </style>
+  
