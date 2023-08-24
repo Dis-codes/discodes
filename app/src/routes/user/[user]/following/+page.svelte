6abd44a3c9bc14f70deb065c5751e5650fa2a441
Nikola <106932301+Nikolafuku@users.noreply.github.com>
2023-08-24 18:39:50 +0200

    Following-ui-update
    - Just some ui changes again...

--- app/src/routes/user/[user]/following/+page.svelte
+++ app/src/routes/user/[user]/following/+page.svelte
@@ -52,27 +52,24 @@
         <div class="flex-1 flex flex-col border border-neutral rounded-xl mb-4">
             <!-- Info content -->
             <div class="flex-1 p-6 shadow-xl">
-                <h1 class="text-4xl font-bold mb-5">{user.username} is following:<br></h1>
+                <h1 class="text-4xl font-bold mb-10">{user.username} is following:</h1>
                 <div class="ml-6">
-                    <div class="stats stats-horizontal mt-3">
+                    <div class="mt-3 flex flex-wrap gap-4">
                         {#if !user.private}
-    <div class="grid grid-cols-2 gap-4">
-        <div class="col-span-1">
-            {#each filteredProfiles as profile (profile.id)}
-                <a href={`/user/${profile.username}`} class="block p-4 bg-white rounded shadow-md mb-4">
-                    <img class="w-16 h-16 rounded-full mx-auto" src={profile.avatar_url} alt={profile.display_name} />
-                    <p class="text-center mt-1">{profile.display_name}</p>
-                    <p class="text-center mt-2 text-gray-500">@{profile.username}</p>
-                    <p class="text-center mt-1 text-cyan-400">
-                        {calculateDaysAgo(profile.logged_at) === 1
-                            ? 'Online'
-                            : `Last online ${calculateDaysAgo(profile.logged_at)} days ago`}
-                    </p>
-                </a>
-            {/each}
-        </div>
-    </div>
-{:else}
+                            {#each filteredProfiles as profile (profile.id)}
+                                <a href={`/user/${profile.username}`} class="flex p-4 rounded shadow-xl mb-4 items-center ring ring-neutral">
+                                    <img class="w-16 h-16 rounded-full ring ring-neutral" src={profile.avatar_url} alt={profile.display_name} />
+                                    <div class="ml-3">
+                                        <p class="text-start">{profile.display_name}</p>
+                                        <p class="text-gray-500">@{profile.username}</p>
+                                        <p class="text-center mt-1 text-cyan-400">
+                                            {calculateDaysAgo(profile.logged_at) === 1
+                                                ? 'Online'
+                                                : `Last online ${calculateDaysAgo(profile.logged_at)} days ago`}
+                                    </p>
+                                </a>
+                            {/each}
+                            {:else}
                         <div class="stat">
                             <div class="stat-title">This user has private profile</div>
                         </div>
