96371e43f25f143f4a540fcc23a698d64a2a79b4
Nikola <106932301+Nikolawut@users.noreply.github.com>
2023-10-14 17:03:16 +0200

    Discover UI (#110)
    * Discover UI
    
    yea
    
    * Did the animz in tailwind css
    
    * added mobile support
    
    ---------
    
    Co-authored-by: Nikola <106932301+Nikolafuku@users.noreply.github.com>
    Co-authored-by: noxyyk <setunskyto@gmail.com>
    Co-authored-by: Noxyyk <me@noxyyk.com>

--- app/src/lib/components/MiscComponents/PluginCard.svelte
+++ app/src/lib/components/MiscComponents/PluginCard.svelte
@@ -1,12 +1,12 @@
 <script>
     export let plugin;
 </script>
-
-<div class="border bg-base-100 shadow-md rounded-lg p-4 m-2 w-72">
+<div class="transform  scale-100  hover:scale-105  transition-transform  duration-500">
+<div class="shadow-xl rounded-lg p-5 m-2 w-52 sm:w-80 ring ring-neutral">
     <div class="font-bold text-xl mb-2 text-gray-300">{plugin.name}</div>
     <p class="text-gray-400 mb-2">{plugin.description}</p>
     <a href="/user/{plugin.user}" class="text-gray-500 hover:link hover:font-bold">By @{plugin.user}</a>
-    <div class="flex justify-between items-center">
+    <div class="flex justify-between sm:items-center flex-col sm:flex-row text-xs sm:text-lg">
         <span class="text-gray-500">Downloads: {plugin.downloads ? plugin.downloads : 0}</span>
         <span class="text-gray-500">Likes: {plugin.rating ? plugin.rating : 0}</span>
         <span class="text-gray-500">Rating: {plugin.rating && plugin.downloads ? (Math.round(plugin.rating/plugin.downloads * 100) / 10) : 0.1}</span>
@@ -16,4 +16,5 @@
             Open
         </a>
     </div>
+</div>
 </div>
